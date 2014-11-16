223c528306404f9a31ef4ecb4241cbc9fdc596d0
carow <carolina.wackerhagen@saturn.uni.freiburg.de>
2014-11-16 12:59:43 +0100

    added publication bias testing

--- Data/R script gibson.R
+++ Data/R script gibson.R
@@ -1,4 +1,5 @@
 setwd("C:/Users/ICI/Desktop/MetaAnalysisD")
+library(metafor)
 table1 <- read.csv2("Gibson.csv")
 str(table1)
 summary(table1)
@@ -14,6 +15,8 @@ attach(table1)
 birds=table1[taxon=="b",-c(5,6,7,8,9,10,11,12,13)]
 detach(table1)
 
+
+
 attach(birds)
 
 library(compute.es)
@@ -26,7 +29,25 @@ effectsizes=mes2(m.1=p.mean, m.2= d.mean,s.pooled=SDpooled, n.1=p.n, n.2=d.n)
 
 detach(birds)
 
+attach(effectsizes)
+
+res1 = rma(yi = g, vi = var.g, method = "DL")
+res
+
+detach(effectsizes)
+
+res1 = rma(yi =effectsizes$g, vi = effectsizes$var.g, method = "DL",  slab=paste(birds$studyID))
+
+forest.rma(res1, annotate=TRUE, cex=0.5 )
 
 
 
 
+#publication bias testing
+funnel(res)
+regtest(res)
+
+
+fsn(yi=g, vi=var.g, data=effectsizes)#By default the Rosentahl Approach "file drawer analysis"
+
+#robustness testing
